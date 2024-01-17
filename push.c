e80e76f0cb16406a36824d33dd8f710e9ac0e16d
kathyslate <mercyoyetunde98@gmail.com>
2024-01-17 08:16:57 -0800

    againnnnnn

--- push.c
+++ push.c
@@ -10,37 +10,20 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-    int n, j = 0;
+    int n = 0;
+    char *endptr;
 
-    if (bus.arg)
-    {
-        if (bus.arg[0] == '-')
-            j++;
-        for (; bus.arg[j] != '\0'; j++)
-        {
-            if (bus.arg[j] > 57 || bus.arg[j] < 48)
-                break;
-        }
-        if (j == 0)
-        {
-            fprintf(stderr, "L%d: usage: push integer\n", line_number);
-            fclose(bus.file);
-            free(bus.content);
-            free_stack(*stack);
-            exit(EXIT_FAILURE);
-        }
-        n = atoi(bus.arg);
-        if (bus.lifi == 0)
-            addnode(stack, n);
-        else
-            addqueue(stack, n);
-    }
-    else
-    {
+    if (bus.arg == NULL || bus.arg[0] == '\0') {
+
         fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        fclose(bus.file);
-        free(bus.content);
-        free_stack(*stack);
         exit(EXIT_FAILURE);
     }
+
+    n = strtol(bus.arg, &endptr, 10);
+    if (endptr == bus.arg) {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    addnode(stack, n);
 }
