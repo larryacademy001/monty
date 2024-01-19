590402ccebdf1c7ca4bc07dc6a003312a1fa2379
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-01-19 03:13:35 +0100

    Task 5

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  * op_nop- nothing
+  * @stack_head: stack head
+  * @count: line_number
+  * Return: Nothing
+ */
+void op_nop(stack_t **stack_head, unsigned int count)
+{
+	(void) count;
+	(void) stack_head;
+}
