8ef7a26ec5df2966f363ba98bb0ccc74ef3ccb32
Larry Academy <olanrewajuawoyemi@gmail.com>
2024-01-19 03:34:52 +0100

    task 7

---
+++ div.c
@@ -0,0 +1,41 @@
+#include "monty.h"
+
+/**
+ * op_div - function that divides the top two elements of the stack.
+ * @stack_head: stack head
+ * @count: line_number
+ * Return: Nothing
+*/
+void op_div(stack_t **stack_head, unsigned int count)
+{
+	stack_t *head;
+	int no_of_elements = 0, temp_p;
+
+	head = *stack_head;
+	while (head)
+	{
+		head = head->next;
+		no_of_elements++;
+	}
+	if (no_of_elements < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", count);
+		fclose(cis.file);
+		free(cis.content);
+		free_the_stack(*stack_head);
+		exit(EXIT_FAILURE);
+	}
+	head = *stack_head;
+	if (head->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", count);
+		fclose(cis.file);
+		free(cis.content);
+		free_the_stack(*stack_head);
+		exit(EXIT_FAILURE);
+	}
+	temp_p = head->next->n / head->n;
+	head->next->n = temp_p;
+	*stack_head = head->next;
+	free(head);
+}
